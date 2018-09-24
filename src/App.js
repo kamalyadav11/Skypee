a16fe3e0ebae0b81578857cac224c5bb932ff26c
kamal11 <kyzx11@gmail.com>
2018-09-24 15:33:07 +0530

    Initial Skypey commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+// import store from "./store";
+
+class App extends Component {
+  render() {
+    return <div className="App">Hello Baba</div>;
+  }
+}
+
+export default App;
