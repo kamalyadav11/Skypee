a99e6459ec9df2c5c8e0808f3190ee9a15d725a3
kamal11 <kyzx11@gmail.com>
2018-09-25 20:17:59 +0530

    Dispatched action in User

--- src/container/User.js
+++ src/container/User.js
@@ -1,10 +1,22 @@
 import React from "react";
+
+import store from "../store";
 import "./User.css";
+import { setActiveUserId } from "../actions";
+
+/**
+ *
+ * whenever the user(you) clicks on any of the Users(in the SideBar) then id of user(in the sidebar) will be set equal to active user_id
+ */
+function handleUserClick({ user_id }) {
+  store.dispatch(setActiveUserId(user_id));
+  console.log(user_id);
+}
 
 const User = ({ user }) => {
   const { name, profile_pic, status } = user;
   return (
-    <div className="User">
+    <div className="User" onClick={handleUserClick.bind(null, user)}>
       <img src={profile_pic} alt={name} className="User__pic" />
       <div className="User__details">
         <p className="User__details-name">{name}</p>
--- src/reducers/ActiveUserID.js
+++ src/reducers/ActiveUserID.js
@@ -1,5 +1,12 @@
+import { SET_ACTIVE_USER_ID } from "../constants/action-types";
+
 const activeUserID = (state = null, action) => {
-  return state;
+  switch (action.type) {
+    case SET_ACTIVE_USER_ID:
+      return action.payload;
+    default:
+      return state;
+  }
 };
 
 export default activeUserID;
